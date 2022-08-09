246da9097710425d67a1bb0524e500651115b30b
Gleb Arkhipov <gleb.arkhipov@genestack.com>
2022-08-09 19:47:38 +0200

    assignment1: implement binary classification metrics

--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -1,24 +1,27 @@
-def binary_classification_metrics(prediction, ground_truth):
+def binary_classification_metrics(predictions, realities):
     '''
     Computes metrics for binary classification
 
     Arguments:
-    prediction, np array of bool (num_samples) - model predictions
-    ground_truth, np array of bool (num_samples) - true labels
+    predictions, np array of bool (num_samples) - model predictions
+    realities, np array of bool (num_samples) - true labels
 
     Returns:
     precision, recall, f1, accuracy - classification metrics
     '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
 
-    # TODO: implement metrics!
-    # Some helpful links:
-    # https://en.wikipedia.org/wiki/Precision_and_recall
-    # https://en.wikipedia.org/wiki/F1_score
-    
+    predictions_and_ground_truths = list(zip(predictions, realities))
+
+    true_positive_num = len(list(filter(lambda x: x[0] == True and x[0] == x[1], predictions_and_ground_truths)))
+    predicted_positive_num = len(list(filter(lambda x: x == True, predictions)))
+    realities_positive_num = len(list(filter(lambda x: x == True, realities)))
+    match_num = len(list(filter(lambda x: x[0] == x[1], predictions_and_ground_truths)))
+
+    precision = true_positive_num / predicted_positive_num
+    recall = true_positive_num / realities_positive_num
+    accuracy = match_num / len(predictions)
+    f1 = 2 * precision * recall / (precision + recall)
+
     return precision, recall, f1, accuracy
 
 
